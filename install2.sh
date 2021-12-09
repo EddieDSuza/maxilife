79fe0a25a3416e07813864c5f1c1d167b80852dd
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2021-12-08 23:27:58 -0300

    initial release

---
+++ install2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Welcome Message
+echo "*******************************************"
+echo "Setting Up Maxilife Hub W/ Zigbee - Ver 1.0"
+echo "*******************************************"
+
+# System Upgrade
+yes | sudo apt-get install 
+yes | sudo apt full-upgrade
+yes | sudo sudo rpi-update
