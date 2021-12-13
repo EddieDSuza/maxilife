5d7c3da6e1bd620bd944755d4482d6377fe35626
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2021-12-13 15:39:25 -0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,23 +5,11 @@ echo "*******************************************"
 echo "Setting Up Maxilife Hub W/ Zigbee - Ver 1.0"
 echo "*******************************************"
 
-if [[ls -l /dev/ttyACM0 = /dev/ttyACM0]]; then
-  echo "Device is connected"
-fi
+sudo apt-get install mosquitto -y
+sudo apt-get install mosquitto-clients
 
 sudo mosquitto_passwd -c /etc/mosquitto/pwfile admin
-read -sp 'Password:' admin;
 
-if [[ $password -ne WHAT GOES HERE? ]]; then
-    MORE CODE HERE
-else
-    MORE CODE HERE
-fi
+read -sp 'Password:' admin
 
-read -sp 'Reenter password:' admin;
-
-if [[ $password -ne WHAT GOES HERE? ]]; then
-    MORE CODE HERE
-else
-    MORE CODE HERE
-fi
+read -sp 'Reenter password:' admin
