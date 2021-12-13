1b1c5ad5db81ad4da324e0c28a7a8526ff4aad45
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2021-12-13 15:07:27 -0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Welcome Message
+echo "*******************************************"
+echo "Setting Up Maxilife Hub W/ Zigbee - Ver 1.0"
+echo "*******************************************"
+
+if [ls -l /dev/ttyACM0 = /dev/ttyACM0]
+  echo "Device is connected"
+else
+  echo "Device is Not connected"
