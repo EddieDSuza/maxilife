734c96c5f7937687a0d2dcdd5a708992521a9e24
Tech With Eddie <52710883+EddieDSuza@users.noreply.github.com>
2021-12-27 12:12:27 -0300

    create tuya.js

---
+++ tuya.js
@@ -0,0 +1,21 @@
+{
+        fingerprint: [{modelID: 'TS0601', manufacturerName: '_TZE200_wv90ladg'}],
+        model: 'HT-09',
+        vendor: 'ETOP',
+        description: 'Wall-mount thermostat',
+        fromZigbee: [fz.legacy.tuya_thermostat_weekly_schedule, fz.etop_thermostat, fz.ignore_basic_report, fz.ignore_tuya_set_time],
+        toZigbee: [tz.etop_thermostat_system_mode, tz.etop_thermostat_away_mode, tz.tuya_thermostat_child_lock,
+            tz.tuya_thermostat_current_heating_setpoint, tz.tuya_thermostat_weekly_schedule],
+        onEvent: tuya.onEventSetTime,
+        meta: {
+            thermostat: {
+                weeklyScheduleMaxTransitions: 4,
+                weeklyScheduleSupportedModes: [1], // bits: 0-heat present, 1-cool present (dec: 1-heat,2-cool,3-heat+cool)
+                weeklyScheduleFirstDayDpId: tuya.dataPoints.schedule,
+            },
+        },
+        exposes: [e.child_lock(), exposes.climate().withSetpoint('current_heating_setpoint', 5, 35, 0.5, ea.STATE_SET)
+            .withLocalTemperature(ea.STATE)
+            .withSystemMode(['off', 'heat', 'auto'], ea.STATE_SET).withRunningState(['idle', 'heat'], ea.STATE)
+            .withAwayMode()],
+    }
